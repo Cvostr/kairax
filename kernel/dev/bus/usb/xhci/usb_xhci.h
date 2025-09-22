d2d442e7b0198d5dd272588e6551a34f3850b652
cvostr <xcvostr@mail.ru>
2025-09-22 20:39:33 +0300

    фикс

--- kernel/dev/bus/usb/xhci/usb_xhci.h
+++ kernel/dev/bus/usb/xhci/usb_xhci.h
@@ -600,12 +600,12 @@ int xhci_device_get_string_language_descriptor(struct xhci_device* dev, struct u
 int xhci_device_get_string_descriptor(struct xhci_device* dev, uint16_t language_id, uint8_t index, struct usb_string_descriptor* descr);
 int xhci_device_get_configuration_descriptor(struct xhci_device* dev, uint8_t configuration, struct usb_configuration_descriptor** descr);
 int xhci_device_set_configuration(struct xhci_device* dev, uint8_t configuration);
+uint8_t xhci_ep_get_absolute_id(struct usb_endpoint_descriptor* descr);
 int xhci_device_handle_transfer_event(struct xhci_device* dev, struct xhci_trb* event);
 int xhci_device_process_configuration(struct xhci_device* device, uint8_t configuration_idx, struct usb_config** config);
 int xhci_device_get_strings_lang_id(struct xhci_device *xhci_device, uint16_t *lang_id);
 int xhci_device_get_product_strings(struct xhci_device* xhci_device, struct usb_device* device);
 int xhci_device_configure_endpoint(struct xhci_device* dev, struct usb_endpoint* endpoint);
-int xhci_device_send_bulk_data(struct xhci_device* dev, struct usb_endpoint* ep, void* data, uint32_t size);
 // функции для указателей
 int xhci_drv_device_send_usb_request(struct usb_device* dev, struct usb_device_request* req, void* out, uint32_t length);
 int xhci_drv_device_configure_endpoint(struct usb_device* dev, struct usb_endpoint* endpoint);
