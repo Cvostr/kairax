7433b35d5443dc1a353f65dccf8c35d81a4e3ef1
cvostr <xcvostr@mail.ru>
2025-12-16 02:31:17 +0300

    исправление

--- kernel/arch/x86_64/kernel.c
+++ kernel/arch/x86_64/kernel.c
@@ -190,8 +190,8 @@ void kmain(uint32_t multiboot_magic, void* multiboot_struct_ptr)
 	pmm_free_pages(multiboot_struct_ptr, align(mb2_info_sz, PAGE_SIZE) / PAGE_SIZE);
 
 	usb_mass_init();
-	usb_hid_kbd_init();
-	usb_hid_mouse_init();
+	//usb_hid_kbd_init();
+	//usb_hid_mouse_init();
 
 	vga_init_dev();
 	random_init();
