301194c746fda4e478d541ced8ed85964adcf29c
cvostr <xcvostr@mail.ru>
2026-05-04 23:22:18 +0300

    доработка утилиты cp

--- progs/copy/copy.c
+++ progs/copy/copy.c
@@ -2,6 +2,7 @@
 #include "errno.h"
 #include "unistd.h"
 #include "fcntl.h"
+#include "string.h"
 
 #define REGION_LEN 4096 * 3
 char region[REGION_LEN];
@@ -19,7 +20,7 @@ int main(int argc, char** argv) {
     int srcfd = open(src, O_RDONLY, 0);
 
     if (srcfd == -1) {
-        printf("Can't open source file %s, code=%i\n", src, errno);
+        printf("cp: can't open source file '%s': %s\n", src, strerror(errno));
         return 2;
     }
 
@@ -27,18 +28,25 @@ int main(int argc, char** argv) {
     int dstfd = creat(dest, 0777);
 
     if (dstfd == -1) {
-        printf("Can't open destination file %s, code=%i\n", dest, errno);
+        printf("cp: can't open destination file '%s': %s\n", dest, strerror(errno));
         return 3;
     }
 
-    ssize_t readed = 0;
-    size_t bytes_written = 0;
-    while ((readed = read(srcfd, region, REGION_LEN)) > 0) {
-        write(dstfd, region, readed);
-        bytes_written += readed;
+    ssize_t readed = 0, written = 0;
+    size_t total_written = 0;
+    while ((readed = read(srcfd, region, REGION_LEN)) > 0) 
+    {
+        written = write(dstfd, region, readed);
+        if (written < 0)
+        {
+            printf("cp: error writing to destination file: %s\n", dest, strerror(errno));
+            return 1;
+        }
+
+        total_written += written;
     }
 
-    printf("File copied, written %i bytes", bytes_written);
+    printf("File copied, written %i bytes", total_written);
 
     close(srcfd);
     close(dstfd);
